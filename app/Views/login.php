10c355d72d2de3aec9913a127cf966da79d6d078
Emmanuel Obeng <emmallob14@gmail.com>
2024-10-01 21:49:52 +0000

    update the index page

--- app/Views/login.php
+++ app/Views/login.php
@@ -3,6 +3,21 @@ include_once 'headtags.php';
 
 global $AppName, $baseURL;
 ?>
+<div class="text-center">
+    <div class="header-container">
+        <h3 class="header-title">
+            Create online surveys and forms that mean business
+        </h3>
+    </div>
+    <p class="header-subtitle">
+        Get better insights faster—with expert templates, helpful AI, and convenient ways to reach more people.
+    </p>
+    <div class="header-image mt-5">
+        <a href="<?= $baseURL ?>auth/signup" class="btn signup-button">
+            <i class="fa fa-user"></i> Sign Up for Free
+        </a>
+    </div>
+</div>
 <div class='dialog-container'>
     <div class='card card-width'>
         <div class='card-content text-center position-relative'>
